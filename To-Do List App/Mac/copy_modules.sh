4508d399aad096fc69f7430406bedd8fe28e3df1
Аббас Гусенов <gusenov@live.ru>
2016-01-29 03:40:25 +0600

    Added script "copy_modules.sh"

---
+++ To-Do List App/Mac/copy_modules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf ./To-Do\ List\ App/To-Do\ List\ App/Lib/PopupInStatusBar/*.*
+cp ./Controls/PopupInStatusBar/PopupInStatusBar/Classes/*.* ./To-Do\ List\ App/To-Do\ List\ App/Lib/PopupInStatusBar/
+
+rm -rf ./To-Do\ List\ App/To-Do\ List\ App/Lib/ToDoListAppUI/*.*
+cp ./Controls/To-Do\ List\ App\ UI/To-Do\ List\ App\ UI/Classes/*.* ./To-Do\ List\ App/To-Do\ List\ App/Lib/ToDoListAppUI/
